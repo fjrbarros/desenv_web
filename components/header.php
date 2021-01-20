7f1a2597ecae1b861a380e690a97b4fa59eae730
fjrbarros <fabio.junior643@outlook.com>
2021-01-19 23:54:40 -0300

    Header

--- components/header.php
+++ components/header.php
@@ -1,11 +1,22 @@
 <head>
     <style>
         header {
-            background-color: tomato;
+            background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("/desenv_web/img/cs1.6.jpg");
+            height: 50%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            position: relative;
         }
 
         header div {
-            padding: 20px
+            padding: 50px;
+            text-align: center;
+            /* position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%); */
+            color: white;
         }
 
         header div ul {
@@ -18,11 +29,12 @@
 <body>
     <header>
         <div>
-            <ul>
+        <h2>Teste</h2>
+            <!-- <ul>
                 <li><a href="#">Link</a></li>
                 <li><a href="#">Link</a></li>
                 <li><a href="#">Link</a></li>
-            </ul>
+            </ul> -->
         </div>
     </header>
 </body>
