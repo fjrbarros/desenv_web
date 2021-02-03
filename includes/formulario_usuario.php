0029762f50d89a97153e37fc28e9117b2bfe2553
fjrbarros <fabio.junior643@outlook.com>
2021-02-03 13:08:58 -0300

    Change in cadastro

--- includes/formulario_usuario.php
+++ includes/formulario_usuario.php
@@ -4,4 +4,25 @@
             <button class="btn btn-success">voltar</button>
         </a>
     </section>
+
+    <h2 class="mt-3">Cadastro de usuário</h2>
+
+    <form method="POST">
+        <div class="form-group">
+            <label>Nome</label>
+            <input type="text" class="form-control" name="nome">
+        </div>
+
+        <div class="form-group">
+            <label>E-mail</label>
+            <input type="email" class="form-control" name="email">
+        </div>
+
+        <div class="form-group">
+            <label>E-mail</label>
+            <input type="email" class="form-control" name="email">
+        </div>
+
+        <input type="submit" value="Entrar">
+    </form>
 </main>
